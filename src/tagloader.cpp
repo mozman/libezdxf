ba016929115453eea691530882aeb9c85fc5bb43
mozman <me@mozman.at>
2020-12-19 22:28:06 +0100

    cmake setup

---
+++ src/tagloader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mozman on 19.12.2020.
+//
+#include "tagloader.hpp"
+
---
+++ src/tagloader.hpp
@@ -0,0 +1,8 @@
+//
+// Created by mozman on 19.12.2020.
+//
+
+#ifndef EZDXF_CPP_TAGLOADER_HPP
+#define EZDXF_CPP_TAGLOADER_HPP
+
+#endif //EZDXF_CPP_TAGLOADER_HPP
