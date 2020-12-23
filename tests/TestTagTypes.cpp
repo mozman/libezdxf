b8e898d125d887fa37bc12602f4e3bbea5ab6a0a
mozman <me@mozman.at>
2020-12-23 13:32:12 +0100

    use data generators for testing

--- tests/TestTagTypes.cpp
+++ tests/TestTagTypes.cpp
@@ -2,41 +2,35 @@
 // License: MIT License
 //
 #include "catch.hpp"
-#include "ezdxf.cpp"
+#include "ezdxf.hpp"
 
 TEST_CASE("Vertex group codes for the x-axis should be VERTEX.",
           "[tag_types]") {
     // A x-axis vertex tag starts the collecting process of 2 or 3 vertex axis.
-    REQUIRE(ezdxf::group_code_type(10) == ezdxf::TagType::VERTEX);
-    REQUIRE(ezdxf::group_code_type(18) == ezdxf::TagType::VERTEX);
-    REQUIRE(ezdxf::group_code_type(210) == ezdxf::TagType::VERTEX);
-    REQUIRE(ezdxf::group_code_type(1010) == ezdxf::TagType::VERTEX);
-    REQUIRE(ezdxf::group_code_type(1013) == ezdxf::TagType::VERTEX);
+    int code = GENERATE(10, 18, 210, 1010, 1013);
+    REQUIRE(ezdxf::group_code_type(code) == ezdxf::TagType::VERTEX);
 }
 
 TEST_CASE("Vertex group codes for y- and z-axis should be DECIMAL.",
           "[tag_types]") {
     // The vertex collecting process should not be started by a y- or z-axis
     // tag, ezdxf consider vertex ordering deviation from xyz as invalid,
-    // but this is valid by the DXF reference -- most (all usable) DXF related
-    // tools write vertices in xyz order! The Python version of ezdxf has a
-    // recover mode which can read files with unordered vertex axis.
-    REQUIRE(ezdxf::group_code_type(20) == ezdxf::TagType::DECIMAL);
-    REQUIRE(ezdxf::group_code_type(30) == ezdxf::TagType::DECIMAL);
-    REQUIRE(ezdxf::group_code_type(220) == ezdxf::TagType::DECIMAL);
-    REQUIRE(ezdxf::group_code_type(1020) == ezdxf::TagType::DECIMAL);
+    // but unordered axis are valid by the DXF reference -- most (all usable)
+    // DXF related tools write vertices in xyz order!
+    // The Python version of ezdxf has a recover mode which can read and
+    // restructure files with unordered vertex axis.
+    int code = GENERATE(20, 30, 220, 230, 1020, 1030);
+    REQUIRE(ezdxf::group_code_type(code) == ezdxf::TagType::DECIMAL);
 }
 
 TEST_CASE("Check group codes of type INTEGER.",
           "[tag_types]") {
-    REQUIRE(ezdxf::group_code_type(60) == ezdxf::TagType::INTEGER);
-    REQUIRE(ezdxf::group_code_type(79) == ezdxf::TagType::INTEGER);
-    REQUIRE(ezdxf::group_code_type(1060) == ezdxf::TagType::INTEGER);
-    REQUIRE(ezdxf::group_code_type(1071) == ezdxf::TagType::INTEGER);
+    int code = GENERATE(60, 79, 1060, 1071);
+    REQUIRE(ezdxf::group_code_type(code) == ezdxf::TagType::INTEGER);
 }
 
 TEST_CASE("Check group codes of type TEXT.",
           "[tag_types]") {
-    REQUIRE(ezdxf::group_code_type(0) == ezdxf::TagType::TEXT);
-    REQUIRE(ezdxf::group_code_type(1000) == ezdxf::TagType::TEXT);
+    int code = GENERATE(0, 9, 100, 1000);
+    REQUIRE(ezdxf::group_code_type(code) == ezdxf::TagType::TEXT);
 }
