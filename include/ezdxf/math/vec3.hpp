90e1bce86c905bd007103b51a9bad902bc25e8cd
mozman <me@mozman.at>
2020-12-27 09:03:02 +0100

    add tests for ezdxf::math::Vec3()

--- include/ezdxf/math/vec3.hpp
+++ include/ezdxf/math/vec3.hpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2020 Manfred Moitzi
 // License: MIT License
-// Fast vector library for usage in C only code - header only!
+//
 #ifndef EZDXF_MATH_VEC3_HPP
 #define EZDXF_MATH_VEC3_HPP
 
@@ -10,9 +10,12 @@
 namespace ezdxf::math {
 
     class Vec3 {
-        // Immutable 3D vector.
+        // Immutable 3D vector or vertex.
 
     private:
+        // Can't use const data members x, y and z, because usage as variable
+        // would not be possible, see test_math_vec3.cpp.
+        //
         double x_;
         double y_;
         double z_;
@@ -30,39 +33,57 @@ namespace ezdxf::math {
 
         [[nodiscard]] double z() const { return z_; }
 
+        [[nodiscard]] bool
+        is_close(const Vec3 &other,
+                 const double &abs_tol = kAbsTol) const {
+            // General comparisons method between Vec3 objects.
+            return ezdxf::math::is_close(x_, other.x_, abs_tol) &&
+                   ezdxf::math::is_close(y_, other.y_, abs_tol) &&
+                   ezdxf::math::is_close(z_, other.z_, abs_tol);
+        }
+
+        [[nodiscard]] bool
+        is_close_zero(const double &abs_tol = kAbsTol) const {
+            // Specialized and faster test for near zero objects.
+            return (fabs(x_) <= abs_tol &&
+                    fabs(y_) <= abs_tol &&
+                    fabs(z_) <= abs_tol);
+        }
+
         Vec3 operator+(const Vec3 &other) const {
             return Vec3(x_ + other.x_, y_ + other.y_, z_ + other.z_);
-        };
+        }
 
         Vec3 operator-(const Vec3 &other) const {
             return Vec3(x_ - other.x_, y_ - other.y_, z_ - other.z_);
-        };
+        }
 
         Vec3 operator*(double factor) const {
             return Vec3(x_ * factor, y_ * factor, z_ * factor);
-        };
+        }
 
         bool operator==(const Vec3 &other) const {
             return is_close(other);
-        };
+        }
 
         bool operator!=(const Vec3 &other) const {
             return !is_close(other);
-        };
+        }
 
-        [[nodiscard]] double magnitude_sqr() const {
+        [[nodiscard]] double magnitude2() const {
             return x_ * x_ + y_ * y_ + z_ * z_;
-        };
+        }
 
         [[nodiscard]] double magnitude() const {
-            return sqrt(x_ * x_ + y_ * y_ + z_ * z_);
+            return sqrt(magnitude2());
         };
 
-        [[nodiscard]] Vec3 normalize(double length) const {
+        [[nodiscard]] Vec3 normalize(double length = 1.0) const {
             double mag = magnitude();
-            if (mag == 0.0) return *this;
-            return *this * (length / mag);
-        };
+            if (fabs(mag) < kAbsTol) return *this;
+            double f = length / mag;
+            return Vec3(x_ * f, y_ * f, z_ * f);
+        }
 
         [[nodiscard]] Vec3 cross(const Vec3 &other) const {
             return Vec3(
@@ -70,28 +91,26 @@ namespace ezdxf::math {
                     z_ * other.x_ - x_ * other.z_,
                     x_ * other.y_ - y_ * other.x_
             );
-        };
+        }
 
         [[nodiscard]] double dot(const Vec3 &other) const {
             return x_ * other.x_ + y_ * other.y_ + z_ * other.z_;
-        };
+        }
 
         [[nodiscard]] double distance(const Vec3 &other) const {
             return (*this - other).magnitude();
-        };
+        }
 
-        [[nodiscard]] Vec3 lerp(const Vec3 &other, double factor) const {
+        [[nodiscard]] Vec3 lerp(const Vec3 &other, double factor = 0.5) const {
             return *this + (other - *this) * factor;
-        };
+        }
 
-        [[nodiscard]] bool
-        is_close(const Vec3 &other,
-                 const double &abs_tol = kAbsTol) const {
-            return ezdxf::math::is_close(x_, other.x_, abs_tol) &&
-                   ezdxf::math::is_close(y_, other.y_, abs_tol) &&
-                   ezdxf::math::is_close(z_, other.z_, abs_tol);
-        };
     };
+
+    const Vec3 X_Axis{1, 0, 0}; // NOLINT(cert-err58-cpp)
+    const Vec3 Y_Axis{0, 1, 0}; // NOLINT(cert-err58-cpp)
+    const Vec3 Z_Axis{0, 0, 1}; // NOLINT(cert-err58-cpp)
+
 }
 
 #endif  // EZDXF_MATH_VEC3_HPP
--- tests/test_math_vec3.cpp
+++ tests/test_math_vec3.cpp
@@ -3,8 +3,45 @@
 //
 #include "catch.hpp"
 #include <ezdxf/math/vec3.hpp>
+#include <vector>
 
 using ezdxf::math::Vec3;
+
+TEST_CASE("Test Vec3 basic usage.", "[math][vec3]") {
+    SECTION("Test access of data members.") {
+        auto v = Vec3{1, 2, 3};
+        REQUIRE(v.x() == 1.0);
+        REQUIRE(v.y() == 2.0);
+        REQUIRE(v.z() == 3.0);
+    }
+
+    SECTION("Test Vec3 as variable.") {
+        auto sum = Vec3{};
+        for (auto d: std::vector<double>{1, 2, 3}) {
+            // Operator "+=" is not implemented, because Vec3() is designed as
+            // immutable data type:
+            sum = sum + Vec3(d, d, d);
+        }
+        REQUIRE(sum == Vec3(6, 6, 6));
+    }
+
+    SECTION("Test Vec3 usage in a vector container.") {
+        auto v = Vec3{1, 2, 3};
+        auto vertices = std::vector<Vec3>{};
+        vertices.push_back(v);
+        vertices.push_back(v);
+        vertices.push_back(v);
+
+        REQUIRE(vertices.size() == 3);
+        REQUIRE(vertices[0] == v);
+
+        // Remove last vertex from container:
+        auto v2 = vertices[2];
+        vertices.pop_back();
+        REQUIRE(v2 == v);
+    }
+}
+
 
 TEST_CASE("Test vector comparisons.", "[math][vec3]") {
     // See also testing of ezdxf::math::is_close in test_math_base.cpp
@@ -12,17 +49,47 @@ TEST_CASE("Test vector comparisons.", "[math][vec3]") {
     SECTION ("Test comparison operators near 0.0") {
         // Default absolute tolerance is 1e-12.
         auto v = Vec3{};
+        const double x = 1e-12;
         REQUIRE(v == Vec3(0, 0, 0));
-        REQUIRE(v == Vec3(1e-12, 0, 0));
-        REQUIRE(v == Vec3(0, 1e-12, 0));
-        REQUIRE(v == Vec3(0, 0, 1e-12));
-        REQUIRE(v.is_close(Vec3(1e-12, 1e-12, 1e-12)) == true);
+        REQUIRE(v == Vec3(x, 0, 0));
+        REQUIRE(v == Vec3(0, x, 0));
+        REQUIRE(v == Vec3(0, 0, x));
+        REQUIRE(v.is_close(Vec3(x, x, x)) == true);
 
         // Leaving the tolerance zone:
-        REQUIRE(v != Vec3(1e-11, 0, 0));
-        REQUIRE(v != Vec3(0, 1e-11, 0));
-        REQUIRE(v != Vec3(0, 0, 1e-11));
-        REQUIRE(v.is_close(Vec3(1e-11, 1e-11, 1e-11)) == false);
+        const double y = 1e-11;
+        REQUIRE(v != Vec3(y, 0, 0));
+        REQUIRE(v != Vec3(0, y, 0));
+        REQUIRE(v != Vec3(0, 0, y));
+        REQUIRE(v.is_close(Vec3(y, y, y)) == false);
+    }
+
+    SECTION ("Test is_close_zero()") {
+        // Default absolute tolerance is 1e-12.
+        const double x = 1e-12;
+        REQUIRE(Vec3().is_close_zero() == true);
+        REQUIRE(Vec3(x, 0, 0).is_close_zero() == true);
+        REQUIRE(Vec3(0, x, 0).is_close_zero() == true);
+        REQUIRE(Vec3(0, 0, x).is_close_zero() == true);
+        REQUIRE(Vec3(x, x, x).is_close_zero() == true);
+    }
+
+    SECTION ("Test not is_close_zero()") {
+        // Default absolute tolerance is 1e-12.
+        const double x = 1e-10;
+        REQUIRE(Vec3(x, 0, 0).is_close_zero() == false);
+        REQUIRE(Vec3(0, x, 0).is_close_zero() == false);
+        REQUIRE(Vec3(0, 0, x).is_close_zero() == false);
+        REQUIRE(Vec3(x, x, x).is_close_zero() == false);
+    }
+
+    SECTION ("Test is_close_zero() with user defined tolerance") {
+        const double usr_tol = 1e-9;
+        const double x = 1e-10;
+        const double y = 1e-8;
+        REQUIRE(Vec3(x, x, x).is_close_zero(usr_tol) == true);
+        REQUIRE(Vec3(usr_tol, 0, 0).is_close_zero(usr_tol) == true);
+        REQUIRE(Vec3(y, y, y).is_close_zero(usr_tol) == false);
     }
 
     SECTION ("Test comparison operators near 1.0") {
@@ -37,24 +104,24 @@ TEST_CASE("Test vector comparisons.", "[math][vec3]") {
         REQUIRE(v.is_close(Vec3(1 + 1e-8, 0, 0)) == false);
     }
 
-    SECTION ("Test is_close() with custom absolute tolerance.") {
+    SECTION ("Test is_close() with user defined tolerance.") {
         // Default relative tolerance is 1e-9:
         auto v = Vec3{1, 1, 1};
-        const double abs_tol = 1e-8;
+        const double usr_tol = 1e-8;
         const double in = 1.0 + 1e-9; // 'in' the tolerance zone
         const double out = 1.0 + 1e-7; // 'out' the tolerance zone
 
-        REQUIRE(v.is_close(Vec3(in, 1, 1), abs_tol) == true);
-        REQUIRE(v.is_close(Vec3(1, in, 1), abs_tol) == true);
-        REQUIRE(v.is_close(Vec3(1, 1, in), abs_tol) == true);
-        REQUIRE(v.is_close(Vec3(in, in, in), abs_tol) == true);
+        REQUIRE(v.is_close(Vec3(in, 1, 1), usr_tol) == true);
+        REQUIRE(v.is_close(Vec3(1, in, 1), usr_tol) == true);
+        REQUIRE(v.is_close(Vec3(1, 1, in), usr_tol) == true);
+        REQUIRE(v.is_close(Vec3(in, in, in), usr_tol) == true);
 
-        REQUIRE(v.is_close(Vec3(1 + abs_tol, 1, 1), abs_tol) == true);
+        REQUIRE(v.is_close(Vec3(1 + usr_tol, 1, 1), usr_tol) == true);
         // Leaving the tolerance zone:
-        REQUIRE(v.is_close(Vec3(out, 1, 1), abs_tol) == false);
-        REQUIRE(v.is_close(Vec3(1, out, 1), abs_tol) == false);
-        REQUIRE(v.is_close(Vec3(1, 1, out), abs_tol) == false);
-        REQUIRE(v.is_close(Vec3(out, out, out), abs_tol) == false);
+        REQUIRE(v.is_close(Vec3(out, 1, 1), usr_tol) == false);
+        REQUIRE(v.is_close(Vec3(1, out, 1), usr_tol) == false);
+        REQUIRE(v.is_close(Vec3(1, 1, out), usr_tol) == false);
+        REQUIRE(v.is_close(Vec3(out, out, out), usr_tol) == false);
     }
 }
 
