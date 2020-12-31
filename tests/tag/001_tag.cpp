d22a9135fb422fe98efd1c0d467525da8435c014
mozman <me@mozman.at>
2020-12-31 08:00:11 +0100

    add scope to all used enums

--- tests/tag/001_tag.cpp
+++ tests/tag/001_tag.cpp
@@ -8,24 +8,24 @@ using namespace ezdxf::tag;
 
 TEST_CASE("Test group codes of type kString.", "[tag]") {
     int16_t code = GENERATE(0, 8, 100, 1000);
-    REQUIRE(group_code_type(code) == kString);
+    REQUIRE(group_code_type(code) == TagType::kString);
 }
 
 TEST_CASE("Test group codes of type kInteger.", "[tag]") {
     int16_t code = GENERATE(60, 70, 79, 1060, 1071);
-    REQUIRE(group_code_type(code) == kInteger);
+    REQUIRE(group_code_type(code) == TagType::kInteger);
 }
 
 TEST_CASE("Test group codes of type kReal.", "[tag]") {
     int16_t code = GENERATE(40, 48, 50, 51, 1014, 1059);
-    REQUIRE(group_code_type(code) == kReal);
+    REQUIRE(group_code_type(code) == TagType::kReal);
 }
 
 TEST_CASE("Test vertex group codes.", "[tag]") {
     SECTION("Vertex x-axis group code is kVec3") {
         // A x-axis vertex tag starts the collecting process of 2 or 3 vertex axis.
         int16_t code = GENERATE(10, 18, 210, 1010, 1013);
-        REQUIRE(group_code_type(code) == kVec3);
+        REQUIRE(group_code_type(code) == TagType::kVec3);
     }
 
     SECTION("Vertex y- and z-axis group codes is kReal") {
@@ -36,14 +36,14 @@ TEST_CASE("Test vertex group codes.", "[tag]") {
         // The Python version of ezdxf has a recover mode which can read and
         // restructure files with unordered vertex axis.
         int16_t code = GENERATE(20, 30, 220, 230, 1020, 1030);
-        REQUIRE(group_code_type(code) == kReal);
+        REQUIRE(group_code_type(code) == TagType::kReal);
     }
 }
 
 TEST_CASE("Test group codes out of defined range are kUndefined.",
           "[tag_types]") {
     int16_t code = GENERATE(-1, 1072);
-    REQUIRE(group_code_type(code) == kUndefined);
+    REQUIRE(group_code_type(code) == TagType::kUndefined);
 
 }
 
@@ -56,35 +56,35 @@ TEST_CASE("Test TagTypeCache", "[tag]") {
 
     SECTION("Test if new cache is empty.") {
         for (int16_t code = 0; code < kGroupCodeCount; code++) {
-            REQUIRE(cache.get(code) == kUndefined);
+            REQUIRE(cache.get(code) == TagType::kUndefined);
         }
     }
     // Fill cache with some data:
-    cache.set(0, kString);
-    cache.set(8, kString);
-    cache.set(10, kVec3);
+    cache.set(0, TagType::kString);
+    cache.set(8, TagType::kString);
+    cache.set(10, TagType::kVec3);
 
     SECTION("Test cache hit.") {
-        REQUIRE(cache.get(0) == kString);
-        REQUIRE(cache.get(8) == kString);
-        REQUIRE(cache.get(10) == kVec3);
+        REQUIRE(cache.get(0) == TagType::kString);
+        REQUIRE(cache.get(8) == TagType::kString);
+        REQUIRE(cache.get(10) == TagType::kVec3);
     }
 
     SECTION("Test cache miss.") {
         int16_t code = GENERATE(1, 11, 1000);
-        REQUIRE(cache.get(code) == kUndefined);
+        REQUIRE(cache.get(code) == TagType::kUndefined);
     }
 
     SECTION("Test if group codes out of defined range are kUndefined.") {
         int16_t code = GENERATE(-1, 1072);
-        REQUIRE(cache.get(code) == kUndefined);
+        REQUIRE(cache.get(code) == TagType::kUndefined);
     }
 }
 
 TEST_CASE("Test undefined DXFTag", "[tag]") {
     auto tag = DXFTag{70};
     SECTION("Test get dedicated value type.") {
-        REQUIRE(tag.type() == kUndefined);
+        REQUIRE(tag.type() == TagType::kUndefined);
         REQUIRE(tag.is_undefined() == true);
         REQUIRE(tag.group_code() == 70);
         REQUIRE(tag.is_error_tag() == false);
@@ -105,7 +105,7 @@ TEST_CASE("Test undefined DXFTag", "[tag]") {
 TEST_CASE("Test StringTag", "[tag]") {
     auto tag = StringTag{0, "LINE"};
     SECTION("Test get dedicated value type.") {
-        REQUIRE(tag.type() == kString);
+        REQUIRE(tag.type() == TagType::kString);
         REQUIRE(tag.group_code() == 0);
         REQUIRE(tag.string() == "LINE");
         REQUIRE(tag.is_error_tag() == false);
@@ -136,7 +136,7 @@ TEST_CASE("Test StringTag", "[tag]") {
 TEST_CASE("Test IntegerTag", "[tag]") {
     auto tag = IntegerTag{70, 16};
     SECTION("Test get dedicated value type.") {
-        REQUIRE(tag.type() == kInteger);
+        REQUIRE(tag.type() == TagType::kInteger);
         REQUIRE(tag.group_code() == 70);
         REQUIRE(tag.integer() == 16);
         REQUIRE(tag.is_error_tag() == false);
@@ -154,7 +154,7 @@ TEST_CASE("Test IntegerTag", "[tag]") {
 TEST_CASE("Test RealTag", "[tag]") {
     auto tag = RealTag{40, 1.0};
     SECTION("Test get dedicated value type.") {
-        REQUIRE(tag.type() == kReal);
+        REQUIRE(tag.type() == TagType::kReal);
         REQUIRE(tag.group_code() == 40);
         REQUIRE(tag.real() == 1.0);
         REQUIRE(tag.is_error_tag() == false);
@@ -172,7 +172,7 @@ TEST_CASE("Test RealTag", "[tag]") {
 TEST_CASE("Test Vec3Tag", "[tag]") {
     auto tag = Vec3Tag{10, 1.0, 2.0, 3.0};
     SECTION("Test get dedicated value type.") {
-        REQUIRE(tag.type() == kVec3);
+        REQUIRE(tag.type() == TagType::kVec3);
         REQUIRE(tag.group_code() == 10);
         REQUIRE(tag.vec3() == Vec3(1.0, 2.0, 3.0));
         REQUIRE(tag.has_vec3_value() == true);
@@ -192,7 +192,7 @@ TEST_CASE("Test Vec2Tag, Vec3 type loaded as 2d type.", "[tag]") {
     auto tag = Vec2Tag{10, 1.0, 2.0};
     SECTION("Test get dedicated value type.") {
         // The following two tests have different results to Vec3Tag.
-        REQUIRE(tag.type() == kVec2);
+        REQUIRE(tag.type() == TagType::kVec2);
         REQUIRE(tag.export_2d() == true);
 
         // Everything else should be equal to Vec3tag:
@@ -217,10 +217,10 @@ TEST_CASE("Store different tag types in a container.", "[tag]") {
     container.push_back(new RealTag(40, 13.0));
 
     REQUIRE(container.size() == 3);
-    REQUIRE(container[0]->type() == kString);
+    REQUIRE(container[0]->type() == TagType::kString);
     REQUIRE(container[0]->string() == "NAME");
-    REQUIRE(container[1]->type() == kInteger);
+    REQUIRE(container[1]->type() == TagType::kInteger);
     REQUIRE(container[1]->integer() == 7);
-    REQUIRE(container[2]->type() == kReal);
+    REQUIRE(container[2]->type() == TagType::kReal);
     REQUIRE(container[2]->real() == 13.0);
 }
